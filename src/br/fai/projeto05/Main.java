52cd9efdc1e2930bd2ef898e3d2b78f4300f308b
gabrieli_coli <gabrielicolisi@hotmail.com>
2020-03-09 11:20:46 -0300

    commit

--- src/br/fai/projeto05/Main.java
+++ src/br/fai/projeto05/Main.java
@@ -3,8 +3,27 @@ package br.fai.projeto05;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Main app = new Main();
+		app.start();
 
+	}
+
+	private void start() {
+		
+		System.out.println("Enviando esta branch para o github");
+		
+		for (int i = 0; i <5; i++) {
+			 switch (i) {
+			 case 1: System.out.println("Pimeira condição");
+				 break;
+			 case 4: System.out.println("Quarta condição");
+				 break;
+				
+		default:
+			System.out.println("Nenhuma condiçao");
+			break;
+			 }
+		}
 	}
 
 }
